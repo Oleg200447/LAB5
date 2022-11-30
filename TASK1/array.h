d8097471079d4979797aad0d566dc1fa33680557
Oleg200447 <113237437+Oleg200447@users.noreply.github.com>
2022-11-30 23:28:11 +0300

    Add files via upload

---
+++ TASK1/array.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void inputChekerElement(int* n, int size);
+
+void inputChekerSize(int* n);
+
+int* memory(int n);
+
+void inputMas(int* mas, int n);
+
+void showMas(int* mas, int n);
+
+void deleteEleven(int** mas, int* n);
